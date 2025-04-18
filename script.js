070f7bbbe377a533c9204d5f9281b4837c86506c
Shelby Norris <shelbs9109@gmail.com>
2025-04-17 22:43:00 -0500

    created html and script files, completed problems 1-7

---
+++ script.js
@@ -0,0 +1,74 @@
+// PART 1: FOR LOOPS
+//Problem 1: Count to 10
+// let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// for (let i = 0; i < numbers.length; i++) {
+//   console.log(numbers[i]);
+// }
+
+//Problem 2: Even Numbers
+// let numTwenty = [
+//   1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+// ];
+
+// for (let i = 1; i < numTwenty.length; i += 2 ) {
+//     console.log(numTwenty[i]);
+// }
+
+//Problem 3: Loop through an Array
+// let animals = ["dog", "cat", "rabbit", "parrot"];
+
+// for (pets of animals) {
+//   console.log("I like " + pets + "s.")
+// }
+
+//Problem 4: Reverse Array Output
+// let colors = ["red", "green", "blue", "yellow"];
+
+// for (let i = colors.length -1; i >= 0; i--) {
+//   console.log(colors[i])
+// }
+
+// PART 2: OBJECTS
+// Problem 5: Simple Object
+
+// let person = {
+//     firstName: "Piper",
+//     age: 20,
+//     favoriteColor: "green"
+// }
+
+// person.hobby = "Drawing"
+
+// console.log(person)
+
+// console.log("My name is " + person.firstName + ". I am " + person.age + " years old and my favorite color is " + person.favoriteColor + ".")
+
+// Problem 6: Modify an Object (add hobby to person object using dot notation)
+
+// Problem 7: Array of Ojects
+
+
+let books = [
+  {
+    title: "Pet Sematary",
+    author: "Stephen King",
+    pages: 416
+  },
+  {
+    title: "The Last Wish",
+    author: "Andrzej Sapkowski",
+    pages: 344
+  },
+  {
+    title: "Angela's Ashes",
+    author: "Frank McCourt",
+    pages: 368
+  }
+]
+
+console.log(books)
+
+
+
+
